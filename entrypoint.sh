3d30bc7fe495c51331acbf9d795e224b76fb99de
Fabian Müller <fmueller@owncloud.com>
2022-04-14 16:18:35 +0200

    Work around fixes implemented for CVE-2022-24765

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# temporary workaround to mitigate the impact of CVE-2022-24765
+# https://github.blog/2022-04-12-git-security-vulnerability-announced/
+# actions are run as root, but the .git dir is owned by some other (non-root user)
+git config --global --add safe.directory /github/workspace
+
 cd "$GITHUB_WORKSPACE"
 
 diff=`git-clang-format --diff --commit $INPUT_BASE`
